1db4f3f505ab621c6d79c5e1e855249ba1689d27
Woolf <woolfson1995@gmail.com>
2025-01-12 22:01:38 +0200

    hw10

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,58 +1,61 @@
+import iziToast from "izitoast";
+
+const inputDelayForm = document.querySelector('.js-input');
+const buttonSubmit = document.querySelector('.js-button');
 
 
 
+const getSelectedState = () => {
+  const selectedRadio = document.querySelector('input[name="state"]:checked');
+  if (selectedRadio) {
+    return selectedRadio.value;
+  }
+  return null;
+}
 
-
-
-
-
-
-// const feedbackFormEl = document.querySelector('.js-feedback-form');
-// let formData = {
-//   email: "",
-//   message: "",
-// };
-
-// const fillFormFields = () => {
-//   try {
-//     const formDataFromLS = JSON.parse(localStorage.getItem('feedback-form-data'));
-//     if (!formDataFromLS) return;
-
-//     formData = formDataFromLS;
+const createPromise = (state, delay) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (state === 'fulfilled') {
+        resolve(`Fulfilled promise in ${delay}ms`);
+      } else if (state === 'rejected') {
+        reject(`Rejected promise in ${delay}ms`);
+      } else {
+        return;
+      }
+    }, delay);
+  })
+};
     
-//     for (const key in formDataFromLS) {
-//       feedbackFormEl.elements[key].value = formDataFromLS[key].trim();
-//     }
-//   } catch (err) {
-//     console.log(err);
-//   }
-// };
 
-// fillFormFields();
+const onCreatePromise = (event) => {
+  event.preventDefault();
+  const state = getSelectedState();
+  const delay = parseInt(inputDelayForm.value, 10);
+  if (!isNaN(delay) && delay >= 0) {
+if (state) {
+    createPromise(state, delay)
+      .then(result => {
+        iziToast.success({
+          message: result
+      })
+      })
+      .catch(error => {
+        iziToast.error({
+          message: error
+        })
+      });
+  }
+}
+  }
+  
+  
 
-// const onFormFieldInput = event => {
-//   const { name, value } = event.target;
-//   formData[name] = value.trim();
+  
 
-//   localStorage.setItem('feedback-form-data', JSON.stringify(formData));
-// };
 
-// const onFeedbackFormSubmit = event => {
-//   event.preventDefault();
-//   for (const key in formData) {
-//     if (!formData[key].trim()) {
-//       alert('Fill please all fields');
-//       return;
-//     }
-//   }
-//     console.log(formData);
-//     event.target.reset();
-//   localStorage.removeItem('feedback-form-data');
-//   formData = {
-//   email: "",
-//   message: "",
-// };
-// };
 
-// feedbackFormEl.addEventListener('input', onFormFieldInput);
-// feedbackFormEl.addEventListener('submit', onFeedbackFormSubmit);
+
+
+
+buttonSubmit.addEventListener('click', onCreatePromise);
